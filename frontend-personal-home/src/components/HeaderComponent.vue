ee65e4c1eed443c4319101295cd9c4d673cae720
liululu <liululu44556@163.com>
2026-05-24 16:43:15 +0800

    ✨ feat: 调整页头顺序

--- frontend-personal-home/src/components/HeaderComponent.vue
+++ frontend-personal-home/src/components/HeaderComponent.vue
@@ -7,12 +7,15 @@
     @select="handleSelect"
   >
     <el-menu-item index="home-page">个人主页</el-menu-item>
-    <el-sub-menu index="project">
+    <el-menu-item index="anime-download">动漫下载</el-menu-item>
+    <el-menu-item index="comic-read">漫画阅读</el-menu-item>
+    <el-menu-item index="website-guide">网址导航</el-menu-item>
+    <!-- <el-sub-menu index="project">
       <template #title>项目</template>
       <el-menu-item index="website-guide">网址导航</el-menu-item>
       <el-menu-item index="anime-download">动漫下载</el-menu-item>
-      <el-menu-item index="comic">漫画阅读</el-menu-item>
-    </el-sub-menu>
+      <el-menu-item index="comic-read">漫画阅读</el-menu-item>
+    </el-sub-menu> -->
     <el-menu-item index="contact-me" >联系我</el-menu-item>
   </el-menu>
 
@@ -22,7 +25,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 
-const activeIndex = ref('1')
+const activeIndex = ref('home-page')
 
 // todo:点击跳转
 const handleSelect = (key: string, keyPath: string[]) => {
@@ -31,7 +34,7 @@ const handleSelect = (key: string, keyPath: string[]) => {
 </script>
 
 <style scoped lang="scss">
-.el-menu--horizontal > .el-menu-item:nth-child(1) {
+.el-menu--horizontal > .el-menu-item:nth-last-child(2) {
   margin-right: auto;
 }
 </style>
